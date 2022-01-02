740440020d1a75336dd0d8c89ae752867e7c7b85
lyncredulous <caitlyn.king2014@gmail.com>
2022-01-02 00:26:41 +0000

    Add UI and update logic

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
-window.onload = () => {
-  function computerPlay(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-
-  function capitalise(str) {
-    return (
-      str.slice(0, 1).toUpperCase() +
-      str.slice(0 - (str.length - 1)).toLowerCase()
-    ).trim();
-  }
-
-  function player(msg, arr) {
-    let playerChoice = capitalise(prompt(msg));
-    if (arr.includes(playerChoice)) {
-      return playerChoice;
-    } else {
-      return player(
-        "Please make a valid choice. Rock, Paper or Scissors:",
-        arr
-      );
-    }
-  }
-
-  function numberofGames(msg = "How many games do you want to play?") {
-    let games = parseInt(prompt(msg));
-    if (isNaN(games)) {
-      return numberofGames("Please type a valid number of games.");
-    } else {
-      return games;
-    }
-  }
-
-  function playRound(playerChoice, computerChoice, winCases) {
-    if (playerChoice === computerChoice) {
-      return "It's a draw!";
-    } else if (winCases[playerChoice] === computerChoice) {
-      return `You win! ${playerChoice} beats ${computerChoice}`;
-    } else {
-      return `Sorry! ${computerChoice} beats ${playerChoice}`;
-    }
-  }
-
-  function game() {
-    const arr = ["Rock", "Paper", "Scissors"];
-    const winCases = { Rock: "Scissors", Paper: "Rock", Scissors: "Paper" };
-    games = numberofGames();
-    for (let i = 0; i < games; i++) {
-      let playerChoice = player("Type Rock, Paper or Scissors:", arr);
-      let computerChoice = computerPlay(arr);
-      console.log(playRound(playerChoice, computerChoice, winCases));
-    }
-  }
-
-  game();
+const buttons = document.querySelectorAll(".rps");
+const arr = ["Rock", "Paper", "Scissors"];
+const winCases = {
+  Rock: "Scissors",
+  Paper: "Rock",
+  Scissors: "Paper",
 };
+const scoreBoard = document.querySelector("#score");
+const results = document.createElement("p");
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function capitalise(str) {
+  return (
+    str.slice(0, 1).toUpperCase() +
+    str.slice(0 - (str.length - 1)).toLowerCase()
+  ).trim();
+}
+
+function player(button) {
+  return button.id;
+}
+
+function playRound(playerChoice, computerChoice, winCases) {
+  if (playerChoice === computerChoice) {
+    return "It's a draw!";
+  } else if (winCases[playerChoice] === computerChoice) {
+    playerScore++;
+    return `You win! ${playerChoice} beats ${computerChoice}`;
+  } else {
+    computerScore++;
+    return `Sorry! ${computerChoice} beats ${playerChoice}`;
+  }
+}
+
+function game(button, arr, winCases) {
+  let playerChoice = player(button);
+  let computerChoice = computerPlay(arr);
+  playRound(playerChoice, computerChoice, winCases);
+}
+
+function main() {
+  buttons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+      game(button, arr, winCases);
+      console.log(playerScore, computerScore);
+      results.textContent = `Player: ${playerScore}
+    Computer: ${computerScore}`;
+      scoreBoard.appendChild(results);
+      if (playerScore === 5) {
+        results.textContent = `Player: ${playerScore}
+        Computer: ${computerScore}.
+        You win!`;
+        playerScore = 0;
+        computerScore = 0;
+      } else if (computerScore === 5) {
+        results.textContent = `Player: ${playerScore}
+        Computer: ${computerScore}.
+        Better luck next time!`;
+        playerScore = 0;
+        computerScore = 0;
+      }
+    })
+  );
+}
+
+main();
